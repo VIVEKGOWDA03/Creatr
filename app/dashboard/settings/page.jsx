6802471e0cd245c818117668afeb473d0a75b34b
Vivek Gowda <vivekck12343@gmail.com>
2025-11-02 22:37:49 +0530

    toast updated

--- app/dashboard/settings/page.jsx
+++ app/dashboard/settings/page.jsx
@@ -18,6 +18,7 @@ import { BarLoader } from "react-spinners";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 const usernameSchema = z.object({
   username: z
     .string()
